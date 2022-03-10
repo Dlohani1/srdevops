7bb0cc7b4e3cc03497c289fe76f05f5fe823ccda
Deepak Lohani <dlohani1@gmail.com>
2022-03-10 16:28:44 +0530

    add function modified

--- add.js
+++ add.js
@@ -1,4 +1,11 @@
-var a
-var b
+function add() {
+var a = document.getElementById("num1").value
+var b = document.getElementById("num2").value
 var result
 result = a + b
+
+document.getElementById("result").value = result
+}
+
+
+
