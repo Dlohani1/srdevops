62eca509ed0923287e5e08cd106683595b1ab71f
Deepak Lohani <dlohani1@gmail.com>
2022-03-10 16:35:38 +0530

    division logic added

---
+++ divide.js
@@ -0,0 +1,8 @@
+function divide () {
+var a = document.getElementById("num1").value
+var b = document.getElementById("num2").value
+var result = a / b
+
+document.getElementById("result").value = result
+
+}
