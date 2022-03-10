84dba09c155c114800c86a744a2eedecc4ed5a32
Deepak Lohani <dlohani1@gmail.com>
2022-03-10 16:31:36 +0530

    subtraction logic added

---
+++ sub.js
@@ -0,0 +1,8 @@
+function sub() {
+var a = document.getElementById("num1").value
+var b = document.getElementById("num2").value
+var result = a - b
+
+document.getElementById("result").value = result
+
+}
