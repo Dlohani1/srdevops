7f547b17e676d87f02f8294937ffd6f5b2be4312
Deepak Lohani <dlohani1@gmail.com>
2022-03-10 17:19:51 +0530

    logic modified

--- add.js
+++ add.js
@@ -2,7 +2,7 @@ function add() {
 var a = document.getElementById("num1").value
 var b = document.getElementById("num2").value
 var result
-result = a + b
+result = parseInt(a) + parseInt(b)
 
 document.getElementById("result").value = result
 }
