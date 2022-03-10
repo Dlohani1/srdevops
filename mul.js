7b2fd64e6f06fc0b151ae8aaaffe27d328adb48a
Deepak Lohani <dlohani1@gmail.com>
2022-03-10 16:33:33 +0530

    multiplication logic added

---
+++ mul.js
@@ -0,0 +1,9 @@
+function mul() {
+var a = document.getElementById("num1").value
+var b = document.getElementById("num2").value
+
+var result = a * b
+
+document.getElementById("result").value = result
+
+}
